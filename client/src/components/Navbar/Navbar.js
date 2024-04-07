d5a495b28b9a44027da292cacfa36a692d1eecdd
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2024-04-07 10:40:47 +0530

    logo-size

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -13,8 +13,6 @@ function Navbar(props) {
 
   const navigate = useNavigate();
   const [scrolling, setScrolling] = useState(false);
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -25,17 +23,11 @@ function Navbar(props) {
       }
     };
 
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    handleResize(); // Initial check on component mount
+    
     window.addEventListener("scroll", handleScroll);
-    window.addEventListener("resize", handleResize);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
-      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -46,10 +38,7 @@ function Navbar(props) {
     });
   };
 
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-    // console.log(sidebarOpen);
-  };
+  
 
   const handleProfileClick = () => {
     if (userType === "Owner") {
@@ -61,12 +50,12 @@ function Navbar(props) {
 
   return (
     <>
-      <div class={`navbar1 ${scrolling ? "solid" : "transparent"}`}>
-        <div class="">
-          <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-0">
+      <div className={`navbar1 ${scrolling ? "solid" : "transparent"}`}>
+        <div className="">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-0">
             <Link
               to=""
-              class="cursor-pointer flex items-center space-x-3 rtl:space-x-reverse"
+              className="cursor-pointer flex items-center space-x-3 rtl:space-x-reverse"
             >
               <img src={IITLogo} alt="Logo" className="logo" />
               {/* <h1 className="navbar-content-heading" onClick={scrollToTop}>
@@ -82,13 +71,13 @@ function Navbar(props) {
             <button
               data-collapse-toggle="navbar-default"
               type="button"
-              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-default"
               aria-expanded="false"
             >
-              <span class="sr-only">Open main menu</span>
+              <span className="sr-only">Open main menu</span>
               <svg
-                class="w-5 h-5"
+                className="w-5 h-5"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -96,21 +85,21 @@ function Navbar(props) {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M1 1h15M1 7h15M1 13h15"
                 />
               </svg>
             </button>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-              <ul class="cursor-pointer font-medium flex flex-col p-4 md:p-0 mt-4  rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  ">
+            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+              <ul className="cursor-pointer font-medium flex flex-col p-4 md:p-0 mt-4  rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  ">
                 {(!props.isLoggedIn || location.pathname === "/") && (
                   <>
                     <li>
                       <Link
                         to="home"
-                        class="block py-2 px-3 text-white rounded md:hover:text-blue-700 md:bg-transparent md:p-0 dark:text-white"
+                        className="block py-2 px-3 text-white rounded md:hover:text-blue-700 md:bg-transparent md:p-0 dark:text-white"
                         aria-current="page"
                         smooth={true}
                       >
@@ -120,7 +109,7 @@ function Navbar(props) {
                     <li>
                       <Link
                         to="about"
-                        class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                         smooth={true}
                       >
                         About
@@ -129,7 +118,7 @@ function Navbar(props) {
                     <li>
                       <Link
                         to="initiatives"
-                        class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                         smooth={true}
                       >
                         Initiatives
@@ -138,7 +127,7 @@ function Navbar(props) {
                     <li>
                       <Link
                         to="advantages"
-                        class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                         smooth={true}
                       >
                         Advantages
@@ -148,7 +137,7 @@ function Navbar(props) {
                       <Link
                         to="track"
                         smooth={true}
-                        class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                       >
                         Track
                       </Link>
@@ -158,7 +147,7 @@ function Navbar(props) {
                 {!props.isLoggedIn && (
                   <li>
                     <button
-                      class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                      className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                       onClick={() => {
                         props.setOpenModal(true);
                       }}
@@ -176,14 +165,14 @@ function Navbar(props) {
                           onClick={() => {
                             navigate("/");
                           }}
-                          class="block py-2 px-5 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                          className="block py-2 px-5 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                         >
                           Home
                         </button>
                       </li>
                       <li>
                         <button
-                          class="block py-2 px-5 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                          className="block py-2 px-5 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                           onClick={() => {
                             navigate("/");
                             localStorage.removeItem("authToken");
@@ -201,7 +190,7 @@ function Navbar(props) {
                   <>
                     <li>
                       <button
-                        class="block py-2 px-5 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                        className="block py-2 px-5 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                         onClick={handleProfileClick}
                       >
                         My Profile
