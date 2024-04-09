b6fc034a8fc5ad6d2159a722cfea7b9117e88af7
MasterEzio <sushant.singh.7326@gmail.com>
2024-04-10 05:28:02 +0530

    removed current and freq graph from user dashboard. added fault and service history

--- client/src/components/UserProfile/UserProfile2.js
+++ client/src/components/UserProfile/UserProfile2.js
@@ -1,15 +1,34 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import "./UserProfile2.css";
 import Box3 from "./blocks/Box3";
-import Box4 from "./blocks/Box4";
-
-import CurrentGraph from "./blocks/CurrentGraph";
-import FreqGraph from "./blocks/FreqGraph";
+import FaultHistory from "../MfPage/blocks/FaultHistory";
+import ServiceHistory from "../MfPage/blocks/ServiceHistory";
 import CarImage from "./blocks/CarImage";
 import DashboardMap from "./blocks/DashboardMap";
 
-const UserProfile2 = () => {
-  const position = [51.505, -0.09];
+const UserProfile2 = (props) => {
+  const [zoom, setZoom] = useState(1);
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      const minWidth = 1024;
+      const maxWidth = 1920;
+      const zoomFactor = Math.min(
+        1,
+        Math.max(
+          0.9,
+          (screenWidth - minWidth) / (maxWidth - minWidth) * (1 - 0.9) + 0.9
+        )
+      );
+      setZoom(zoomFactor);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize(); // Run on initial render
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <div className="user-profile-2">
@@ -23,13 +42,27 @@ const UserProfile2 = () => {
         <Box3 />
       </div>
       <div className="user-profile-2-card-1">
-        {/* <Box4 /> */}
+        {/* <Box4 />
         <div className="box4">
           <div className="cr-graph">
             <CurrentGraph />
           </div>
           <div className="fr-graph">
             <FreqGraph />
+          </div>
+        </div> */}
+        <div className="mf-page-box-4">
+          <div className="mf-page-card-2">
+            <FaultHistory
+              notifyerror={props.notifyerror}
+              notifysuccess={props.notifysuccess}
+            />
+          </div>
+          <div className="mf-page-card-2">
+            <ServiceHistory
+              notifyerror={props.notifyerror}
+              notifysuccess={props.notifysuccess}
+            />
           </div>
         </div>
       </div>
